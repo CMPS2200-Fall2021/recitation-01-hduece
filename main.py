9986c5e2b6cfc5b86207e9545bff4fbccae5a362
hduece <77637496+hduece@users.noreply.github.com>
2021-09-15 22:24:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -99,7 +99,8 @@ def compare_search(sizes=[1e1, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7]):
 		rangeList = list(range(i))
 		lineartime = time_search(linear_search, rangeList, -1)
 		binarytime = time_search(binary_search, rangeList, -1)
-		results+= (i, lineartime, binarytime) 
+		conclusion = (i,lineartime,binarytime)
+		results.append(conclusion) 
 	return(results)
 
 def print_results(results):
